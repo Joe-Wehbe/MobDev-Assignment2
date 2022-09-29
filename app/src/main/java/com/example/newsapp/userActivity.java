49f7c8c4ccbd4db34d873a7608a182d1b35602a4
Joe-Wehbe <joewehbe@yahoo.com>
2022-09-30 02:51:59 +0300

    commit #2: splash screen and userActivity

---
+++ app/src/main/java/com/example/newsapp/userActivity.java
@@ -0,0 +1,17 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        getSupportActionBar().hide();
+
+    }
+}
