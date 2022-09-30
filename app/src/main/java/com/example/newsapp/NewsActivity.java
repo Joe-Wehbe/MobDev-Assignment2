07c3abe55b5b73c2a7578f42ff0007bd27471113
Joe-Wehbe <joewehbe@yahoo.com>
2022-09-30 15:18:38 +0300

    commit #3: Coded shared preferences

---
+++ app/src/main/java/com/example/newsapp/NewsActivity.java
@@ -0,0 +1,20 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+    }
+}
--- app/src/main/java/com/example/newsapp/userActivity.java
+++ app/src/main/java/com/example/newsapp/userActivity.java
@@ -1,10 +1,21 @@
 package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class userActivity extends AppCompatActivity {
+
+    EditText name;
+    Button newsBtn;
+    SharedPreferences sp;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +23,32 @@ public class userActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user);
 
         getSupportActionBar().hide();
+
+        name = findViewById(R.id.nameInput);
+        newsBtn = findViewById(R.id.checkNewsButton);
+        sp = getSharedPreferences("usernamePrefs", Context.MODE_PRIVATE);
+    }
+
+    public void checkNews(View v){
+
+        username = name.getText().toString();
+
+        if(!username.equals("")){
+            Toast.makeText(getApplicationContext(), "Welcome, " +  username + "!", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(getApplicationContext(), NewsActivity.class);
+            startActivity(i);
+
+            newsBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("name", username);
+                    editor.commit();
+                }
+            });
+        }else{
+            Toast.makeText(getApplicationContext(), "Please enter your name!", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
